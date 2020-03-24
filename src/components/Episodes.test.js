4bd7865a914a48c4508270906514c0da0941b5b9
Scott Smith <scthsm@outlook.com>
2020-03-23 19:05:02 -0600

    a

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -3,55 +3,85 @@ import {render, queryByTestId} from "@testing-library/react";
 import Episodes from "./Episodes";
 
 
-test ("renders without erros",()=> {
-    const {} = render(<Episodes episodes={[]} error="" />);
+test ("renders",()=> {
+    const {} = render(<Episodes episodes={[]}/>);
 
 })
 
-test("renders error message if error is present", () => {
-    // arrange, act, assert all at once
-    const { queryByTestId, rerender, debug } = render(
-      <MissionsList missions={[]} error="" />
-    );  // assert that no error message is rendered
-    // query for the element (Should return null)
-    // assert with .toBeNull()
-    expect(queryByTestId(/error-message/i)).toBeNull();  // rerender the component with new props (errors)
-    rerender(<MissionsList missions={[]} error="Some error message" />);  // assert that the error message is rendered
-    expect(queryByTestId(/error-message/i)).toBeInTheDocument();
-  });
 
-
-  test("renders misssions when missions is mpresnts", () => {
+  test("renders episodes", () => {
    
-    const { queryByTestId, rerender, debug } = render(
-      <MissionsList missions={[]} error="" />
+    const { queryByTestId, rerender, debug, getAllByTestId } = render(
+      <Episodes episodes={[]}  />
     );  
-    const mockMissionsData = [
+
+    const mockEpisodesData = [
         {
-          mission_name: "Thaicom",
-          mission_id: "9D1B7E0",
-          manufacturers: ["Orbital ATK"],
-          payload_ids: ["Thaicom 6", "Thaicom 8"],
-          wikipedia: "https://en.wikipedia.org/wiki/Thaicom",
-          website: "http://www.thaicom.net/en/satellites/overview",
-          twitter: "https://twitter.com/thaicomplc",
-          description:
-            "Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology."
+          id: 553946,
+          url: 'http://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers',
+          name: 'Chapter One: The Vanishing of Will Byers',
+          season: 1,
+          number: 1,
+          airdate: '2016-07-15',
+          airtime: '',
+          airstamp: '2016-07-15T12:00:00+00:00',
+          runtime: 60,
+          image: {
+            medium: 'http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg',
+            original: 'http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg'
+          },
+          summary: '<p>A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy\'s friends conduct their own search, and meet a mysterious girl in the forest.</p>',
+          _links: {
+            self: {
+              href: 'http://api.tvmaze.com/episodes/553946'
+            }
+          }
         },
         {
-          mission_name: "Telstar",
-          mission_id: "F4F83DE",
-          manufacturers: ["SSL"],
-          payload_ids: ["Telstar 19V", "Telstar 18V"],
-          wikipedia: "https://en.wikipedia.org/wiki/Telesat",
-          website: "https://www.telesat.com/",
-          twitter: null,
-          description:
-            "Telstar 19V (Telstar 19 Vantage) is a communication satellite in the Telstar series of the Canadian satellite communications company Telesat. It was built by Space Systems Loral (MAXAR) and is based on the SSL-1300 bus. As of 26 July 2018, Telstar 19V is the heaviest commercial communications satellite ever launched, weighing at 7,076 kg (15,600 lbs) and surpassing the previous record, set by TerreStar-1 (6,910 kg/15230lbs), launched by Ariane 5ECA on 1 July 2009."
+          id: 578663,
+          url: 'http://www.tvmaze.com/episodes/578663/stranger-things-1x02-chapter-two-the-weirdo-on-maple-street',
+          name: 'Chapter Two: The Weirdo on Maple Street',
+          season: 1,
+          number: 2,
+          airdate: '2016-07-15',
+          airtime: '',
+          airstamp: '2016-07-15T12:00:00+00:00',
+          runtime: 60,
+          image: {
+            medium: 'http://static.tvmaze.com/uploads/images/medium_landscape/72/181376.jpg',
+            original: 'http://static.tvmaze.com/uploads/images/original_untouched/72/181376.jpg'
+          },
+          summary: '<p>While the search for the missing Will continues, Joyce tells Jim about a call she apparently received from her son. Meanwhile, Jane warns Mike that there are bad people after her, and he realizes that she knows what happened to Will.</p>',
+          _links: {
+            self: {
+              href: 'http://api.tvmaze.com/episodes/578663'
+            }
+          }
+        },
+        {
+          id: 578664,
+          url: 'http://www.tvmaze.com/episodes/578664/stranger-things-1x03-chapter-three-holly-jolly',
+          name: 'Chapter Three: Holly, Jolly',
+          season: 1,
+          number: 3,
+          airdate: '2016-07-15',
+          airtime: '',
+          airstamp: '2016-07-15T12:00:00+00:00',
+          runtime: 60,
+          image: {
+            medium: 'http://static.tvmaze.com/uploads/images/medium_landscape/67/168920.jpg',
+            original: 'http://static.tvmaze.com/uploads/images/original_untouched/67/168920.jpg'
+          },
+          summary: '<p>While Nancy looks for a missing Barbara and realizes that Jonathan may have been the last person to see her, Mike and his friends go out with Jane to find the missing Will. Meanwhile, Jim tracks Will to the lab.</p>',
+          _links: {
+            self: {
+              href: 'http://api.tvmaze.com/episodes/578664'
+            }
+          }
         }
-      ];
+    ];
       
-    expect(queryByTestId(/mission/i)).toBeNull();  // rerender the component with new props (errors)
-    rerender(<MissionsList missions={mockMissionsData} error="" />);  // assert that the error message is rendered
-    expect(queryByTestId(/mission/i)).toBeInTheDocument();
+    expect(queryByTestId(/episode/i)).toBeNull();  // rerender the component with new props (errors)
+    rerender(<Episodes episodes={mockEpisodesData}/>);  // assert that the error message is rendered
+    expect(getAllByTestId(/episode-list/i)).toHaveLength(3);
   });
